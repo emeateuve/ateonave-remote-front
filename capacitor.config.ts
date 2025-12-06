a40750de09c6091c122f2f98aa8683989631c8f2
emeateuve <emeateuve@gmail.com>
2025-12-06 21:45:16 +0100

    Added android upload (#1)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.miempresa.ateonave',
+  appName: 'Ateonave Remote',
+  webDir: 'dist/ateonave-remote-front/browser',
+};
+
+export default config;
